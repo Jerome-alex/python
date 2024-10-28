82e2f3a0a2d070cafa8fce9aafc700d3c11da21f
Jerome-alex <137639468+Jerome-alex@users.noreply.github.com>
2024-10-28 10:00:19 +0530

    Update mnprime.py

--- mnprime.py
+++ mnprime.py
@@ -1,12 +1,21 @@
-def isprime(n):
-	if n<=1:
-		return False
-	for i in range(2,n):
-		if n%i==0:
-			return False
-	return True
-n=int(input("enter a number"))
-for i in range(1,n):
-	if isprime(i):
-		print(i)
+def is_prime(num):
+    if num < 2:
+        return False
+    for i in range(2, int(num**0.5) + 1):
+        if num % i == 0:
+            return False
+    return True
+
+def first_n_primes(n):
+    primes = []
+    candidate = 2  # Start checking for primes from 2
+    while len(primes) < n:
+        if is_prime(candidate):
+            primes.append(candidate)
+        candidate += 1
+    return primes
+
+# Get user input for the number of primes to generate
+n = int(input("Enter the number of prime numbers to generate: "))
+print(first_n_primes(n))
 
